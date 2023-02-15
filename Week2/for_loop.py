e1f8625faac15f87828821640f165bdce2295906
samaraorega <terrysamara98@gmail.com>
2023-02-15 11:37:56 +0300

    for loops

--- Week2/for_loop.py
+++ Week2/for_loop.py
@@ -1,2 +1,17 @@
-for i in range(0,7):
-    print(i)
+sum = 0
+for i in range(0,10):
+    sum = sum + i
+
+    print(sum)
+
+    product = 1
+    for s in range(1,10):
+        product = product * s
+
+    print(product)
+
+    #factorial
+    #6! = 6*5*4*3*2*1
+    
+    
+    
